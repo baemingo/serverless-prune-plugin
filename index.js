5c63fb0bb2e44c798b04c72c68cf19a13e4481ba
Sergey <tgradesome@gmail.com>
2021-06-25 05:04:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ class Prune {
   prune() {
 
     const selectedFunctions = this.options.function ? [this.options.function] : this.serverless.service.getAllFunctions();
-    const functionNames = selectedFunctions.map(key => this.serverless.service.getFunction(key).name || `${this.serverless.service.provider.stage}-${key}`);
+    const functionNames = selectedFunctions.map(key => this.serverless.service.getFunction(key).name || `${this.serverless.service.name}-${key}`);
 
     this.serverless.cli.log('Prune: Querying for deployed function versions');
 
